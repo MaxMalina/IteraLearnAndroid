837b5f4b23127770ff56801e1c9f1bb1cb1ef08b
Max <max.malina13@gmail.com>
2018-02-19 11:17:08 +0200

    Fix pull to refresh

--- app/src/main/java/com/learn/maksymgromov/learnui/Fragments/DashboardFragment.java
+++ app/src/main/java/com/learn/maksymgromov/learnui/Fragments/DashboardFragment.java
@@ -4,15 +4,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.learn.maksymgromov.learnui.Adapters.DashboardAdapter;
 import com.learn.maksymgromov.learnui.Model.Car;
@@ -40,6 +36,7 @@ public class DashboardFragment extends Fragment implements SwipeRefreshLayout.On
 
         ButterKnife.bind(this, view);
 
+        mSwipeRefreshLayout.setRefreshing(true);
         mSwipeRefreshLayout.setOnRefreshListener(this);
 
         mDashboardRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -52,12 +49,10 @@ public class DashboardFragment extends Fragment implements SwipeRefreshLayout.On
 
     @Override
     public void onRefresh() {
-        mDashboardRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mSwipeRefreshLayout.setRefreshing(true);
 
         requestClient = new RequestClient();
         new RequestTask().execute();
-
-        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     class RequestTask extends AsyncTask<String, Integer, String>  {
@@ -68,14 +63,11 @@ public class DashboardFragment extends Fragment implements SwipeRefreshLayout.On
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
         protected void onPostExecute(String string) {
             mData = Utils.parseJsonStringToArrayListString(string);
             mDashboardRecyclerView.setAdapter(new DashboardAdapter(mData));
+
+            mSwipeRefreshLayout.setRefreshing(false);
         }
     }
 }
